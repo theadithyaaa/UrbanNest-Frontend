7fa1f43ef621b299d25da1f7d9fe1ff2837dfde7
theadithyaaa <malsha.adithya95@gmail.com>
2024-11-05 12:38:47 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeroPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
