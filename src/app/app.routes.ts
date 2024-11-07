5c481212506156ed53cedafdd0fccfe596deca8e
theadithyaaa <malsha.adithya95@gmail.com>
2024-11-07 10:56:35 +0530

    modals completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { CustomerDashboardComponent } from './pages/customer-dashboard/customer-dashboard.component';
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
+import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
 
 export const routes: Routes = [
     {
@@ -11,6 +12,10 @@ export const routes: Routes = [
         path: "customer-dashboard",
         component: CustomerDashboardComponent
     },
+    {
+        path: "admin-dashboard",
+        component: AdminDashboardComponent
+    }
     
     
 ];
