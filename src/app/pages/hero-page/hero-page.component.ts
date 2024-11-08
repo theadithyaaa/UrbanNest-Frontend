2956bda2d797a9adac8a62de22b94775dc069c87
theadithyaaa <malsha.adithya95@gmail.com>
2024-11-08 14:27:22 +0530

    admin login authntication

--- src/app/pages/hero-page/hero-page.component.ts
+++ src/app/pages/hero-page/hero-page.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-hero-page',
   standalone: true,
-  imports: [RouterLink, FormsModule],
+  imports: [RouterLink, FormsModule , FormsModule],
   templateUrl: './hero-page.component.html',
   styleUrls: ['./hero-page.component.css'] 
 })
 export class HeroPageComponent {
 
+  email: string = "";
+  password: string = "";
+
+  login() {
+    if (this.email === 'admin@gmail.com' && this.password === 'admin123') {
+      alert('Login successful');
+      window.location.href = '/admin-dashboard';
+    } else {
+      alert('Invalid email or password');
+    }
+  }
+
 }
+
